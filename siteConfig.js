2b79cd31bbd45643f7b29bf5de63d8f2dde86c98
Leon <l-hofmann@outlook.de>
2020-07-06 22:19:28 +0200

    Adding home quote

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: "Brunhild Hofmann",
   shortName: "Brunhild Hofmann",
-  description: "Diese Seite befindet sich im Aufbau...",
+  description:
+    "Ach, dass wir doch dem reinen stillen Wink des Herzens nachzugehen so sehr verlernen! Ganz leise spricht ein Gott in unsrer Brust, ganz leise, ganz vernehmlich, zeigt uns an, was zu ergreifen ist und was zu fliehn.",
   url: "https:/brunhildhofmann.de",
   prefix: "/",
   author: "Brunhild Hofmann",
